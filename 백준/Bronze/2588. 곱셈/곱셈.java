7c9f79d071804c4a946099850756f07b78562a3e
Sean K <122565412+seandoesdev@users.noreply.github.com>
2025-07-27 15:11:12 +0900

    [Bronze III] Title: 곱셈, Time: 112 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -1,34 +1,33 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void process(String a, String b) {
+
         int a_num = Integer.parseInt(a);
         int b_num = Integer.parseInt(b);
-        List<Integer> b_list = toList(b);
+        List<Integer> b_list = b.chars()
+                                .map(c-> c-'0')
+                                .boxed()
+                                .collect(Collectors.toList());
+
         for(int i=b_list.size(); i>0; i--){
-            System.out.print(b_list.get(i-1) * a_num + "\n");
+            System.out.println(a_num * b_list.get(i-1));
         }
+
         System.out.println(a_num * b_num);
-    }
-
-    // 문자열 처리
-    public static List<Integer> toList(String numStr){
-        List<Integer> list = new ArrayList<>();
-
-        for(int i=0; i<numStr.length(); i++){
-            list.add(numStr.charAt(i) - '0');
-        }
-        return list;
     }
 
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        process(br.readLine(), br.readLine());
+
+        String a = br.readLine();
+        String b = br.readLine();
+
+        process(a, b);
     }
 
 }

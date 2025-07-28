f07f7047c26279950c6e584757ead5501a31e89d
Sean K <122565412+seandoesdev@users.noreply.github.com>
2025-07-28 22:43:28 +0900

    [Bronze III] Title: 오븐 시계, Time: 120 ms, Memory: 15952 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,21 +1,21 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int hour = Integer.parseInt(st.nextToken());
-        int min = Integer.parseInt(st.nextToken());
-        int time = Integer.parseInt(br.readLine());
+        String time = br.readLine();
+        
+        int hour = Integer.parseInt(time.split(" ")[0]);
+        int min = Integer.parseInt(time.split(" ")[1]);
+        int cookingTime = Integer.parseInt(br.readLine());
 
-        if(min + time >= 60){
-            hour += (min + time) / 60;
-            min = (min + time) % 60;
+        if(min + cookingTime >= 60){
+            hour += (min + cookingTime) / 60;
+            min = (min + cookingTime) % 60;
         }else{
-            min += time;
+            min += cookingTime;
         }
         if(hour >= 24){
             hour = hour % 24;

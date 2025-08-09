a2fbeaf87d4efb38390db2c506a3510e11df245d
Sean K <122565412+seandoesdev@users.noreply.github.com>
2025-08-09 21:28:32 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        int idx = num_list.length - 1;
+        for(int i : num_list){
+            answer[idx] = i;
+            idx--;
+        }
+        return answer;
+    }
+}
